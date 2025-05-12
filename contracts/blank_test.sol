9d575ba31b34bc2a4c017d1ca798f94045db436d
zqdfound <15950515069@163.com>
2025-05-12 21:36:10 +0800

    init

--- contracts/blank_test.sol
+++ contracts/blank_test.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
+contract HelloWorld{
+    string strVal = "Hello World";
+    function sayHello() public view returns (string memory){
+        return strVal;
+    }
+    function setHello(string memory helloStr) public {
+        strVal = helloStr;
+    }
+    
+}
